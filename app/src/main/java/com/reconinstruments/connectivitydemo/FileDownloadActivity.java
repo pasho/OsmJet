ec2cf1afcd31ea0c86550e31695fdcd92956f50b
Pavel <pfedulov84@gmail.com>
2016-06-25 18:08:19 +0100

    Init. Based on connectivity sample

---
+++ app/src/main/java/com/reconinstruments/connectivitydemo/FileDownloadActivity.java
@@ -0,0 +1,100 @@
+package com.reconinstruments.connectivitydemo;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.reconinstruments.os.HUDOS;
+import com.reconinstruments.os.connectivity.HUDConnectivityManager;
+import com.reconinstruments.os.connectivity.http.HUDHttpRequest;
+import com.reconinstruments.os.connectivity.http.HUDHttpResponse;
+
+public class FileDownloadActivity extends Activity
+{
+    public static TextView mNumberOfRequest;
+    public static TextView mNumberOfGoodResponse;
+    public static TextView mNumberOfBadResponse;
+    public static TextView mCommentTextView;
+
+    public static int mRequestCounter = 0;
+    public static int mGoodCounter = 0;
+    public static int mBadCounter = 0;
+
+    private HUDConnectivityManager mHUDConnectivityManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.file_layout);
+
+        mNumberOfRequest = (TextView) findViewById(R.id.numberOfRequest);
+        mNumberOfGoodResponse = (TextView) findViewById(R.id.numberOfGoodResponse);
+        mNumberOfBadResponse = (TextView) findViewById(R.id.numberOfBadResponse);
+        mCommentTextView = (TextView) findViewById(R.id.commentTextView);
+
+        mHUDConnectivityManager = (HUDConnectivityManager) HUDOS.getHUDService(HUDOS.HUD_CONNECTIVITY_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String url = "http://www.reconinstruments.com/wp-content/themes/recon/img/jet/slide-3.jpg";
+
+        mRequestCounter = 0;
+        mGoodCounter = 0;
+        mBadCounter = 0;
+
+        mNumberOfRequest.setText("0");
+        mNumberOfGoodResponse.setText("0");
+        mNumberOfBadResponse.setText("0");
+
+        new DownloadFileTask(url).execute();
+    }
+
+    private class DownloadFileTask extends AsyncTask<Void, Void, Boolean> {
+
+        String mUrl;
+        String mComment;
+
+        public DownloadFileTask(String url) {
+            mUrl = url;
+            mRequestCounter ++;
+            mNumberOfRequest.setText(Integer.toString(mRequestCounter));
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            boolean result = false;
+            try {
+
+                //Get Request
+                HUDHttpRequest request = new HUDHttpRequest(HUDHttpRequest.RequestMethod.GET, mUrl);
+                HUDHttpResponse response = mHUDConnectivityManager.sendWebRequest(request);
+                if (response.hasBody()) {
+                    mComment = "response bodySize:" + response.getBody().length;
+                    result = true;
+                }
+            } catch (Exception e) {
+                mComment = "failed to download file: " + e.getMessage();
+                e.printStackTrace();
+                return false;
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            if(result){
+                mGoodCounter ++;
+            }else {
+                mBadCounter ++;
+            }
+            mNumberOfGoodResponse.setText(Integer.toString(mGoodCounter));
+            mNumberOfBadResponse.setText(Integer.toString(mBadCounter));
+            mCommentTextView.setText(mComment);
+        }
+    }
+}
---
+++ app/src/main/java/com/reconinstruments/connectivitydemo/ImageDownloadActivity.java
@@ -0,0 +1,106 @@
+package com.reconinstruments.connectivitydemo;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.reconinstruments.os.HUDOS;
+import com.reconinstruments.os.connectivity.HUDConnectivityManager;
+import com.reconinstruments.os.connectivity.http.HUDHttpRequest;
+import com.reconinstruments.os.connectivity.http.HUDHttpResponse;
+
+public class ImageDownloadActivity extends Activity
+{
+    private final String TAG = this.getClass().getSimpleName();
+    public static String EXTRA_IMAGE_URL = "IMAGE_URL";
+
+    private HUDConnectivityManager mHUDConnectivityManager;
+    private ImageView mImage = null;
+    private int mDialogRefCnt = 0;
+    private ProgressDialog mDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        mDialog = new ProgressDialog(this);
+        mDialog.setMessage("Downloading...");
+        setContentView(R.layout.image_layout);
+        mImage = (ImageView) findViewById(R.id.imageView);
+        mHUDConnectivityManager = (HUDConnectivityManager) HUDOS.getHUDService(HUDOS.HUD_CONNECTIVITY_SERVICE);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null)
+        {
+            String url = extras.getString(EXTRA_IMAGE_URL);
+            new DownloadHUDImageTask(mImage).execute(url);
+        }
+        else{ Log.e(TAG, "No url was received from Intent"); }
+    }
+
+    private class DownloadHUDImageTask extends AsyncTask<String, Void, Bitmap>
+    {
+        ImageView bmImage;
+
+        public DownloadHUDImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        @Override
+        protected void onPreExecute()
+        {
+            synchronized (this)
+            {
+                if (mDialogRefCnt == 0){ mDialog.show(); }
+                mDialogRefCnt++;
+            }
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... urls)
+        {
+            String urlDisplay = urls[0];
+            Bitmap bitmapImg = null;
+            try
+            {
+                //Http Get Request
+                HUDHttpRequest request = new HUDHttpRequest(HUDHttpRequest.RequestMethod.GET, urlDisplay);
+                HUDHttpResponse response = mHUDConnectivityManager.sendWebRequest(request);
+                if (response.hasBody())
+                {
+                    byte[] data = response.getBody();
+                    bitmapImg = BitmapFactory.decodeByteArray(data, 0, data.length);
+                }
+            }
+            catch (Exception e){ e.printStackTrace(); }
+            return bitmapImg;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result)
+        {
+            bmImage.setImageBitmap(result);
+            synchronized (this)
+            {
+                if (mDialogRefCnt <= 1)
+                {
+                    if (mDialog.isShowing()){ mDialog.dismiss(); }
+                    mDialogRefCnt = 0;
+                }
+                else { mDialogRefCnt--; }
+            }
+        }
+    }
+}
