3fbd9591cb14b8b4ac61116da3b89869824545d1
Pavel <pfedulov84@gmail.com>
2016-06-26 23:07:34 +0100

    Code cleanup

--- app/src/main/java/com/pasho/osmmap/MapActivity.java
+++ app/src/main/java/com/pasho/osmmap/MapActivity.java
@@ -1,59 +1,41 @@
 package com.pasho.osmmap;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
-import android.graphics.RectF;
-import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 
 import com.reconinstruments.os.HUDOS;
 import com.reconinstruments.os.connectivity.HUDConnectivityManager;
-import com.reconinstruments.os.connectivity.http.HUDHttpRequest;
-import com.reconinstruments.os.connectivity.http.HUDHttpResponse;
 import com.reconinstruments.os.hardware.sensors.HUDHeadingManager;
 import com.reconinstruments.os.hardware.sensors.HeadLocationListener;
 
-import java.util.Locale;
-
 public class MapActivity extends Activity implements HeadLocationListener, ITilesConsumer {
     private final String TAG = this.getClass().getSimpleName();
-    public static String EXTRA_IMAGE_URL = "IMAGE_URL";
 
-    private HUDConnectivityManager mHUDConnectivityManager;
-    private HUDHeadingManager mHUDHeadingManager;
-    private ImageView mImage = null;
-    private int mDialogRefCnt = 0;
-    private ProgressDialog mDialog;
-    private LocationManager mLocationService;
-    private String tileUrl = "";
-    private int[] _currentTile = new int[0];
+    private HUDConnectivityManager connectivityManager;
+    private HUDHeadingManager headingManager;
+    private ImageView imageView = null;
+    private LocationManager locationService;
     private TilesManager tileManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mDialog = new ProgressDialog(this);
-        mDialog.setMessage("Downloading...");
         setContentView(R.layout.image_layout);
 
-        mImage = (ImageView) findViewById(R.id.imageView);
-        mImage.setScaleType(ImageView.ScaleType.MATRIX);
+        imageView = (ImageView) findViewById(R.id.imageView);
 
-        mHUDConnectivityManager = (HUDConnectivityManager) HUDOS.getHUDService(HUDOS.HUD_CONNECTIVITY_SERVICE);
-        mLocationService = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        mHUDHeadingManager = (HUDHeadingManager) HUDOS.getHUDService(HUDOS.HUD_HEADING_SERVICE);
-        tileManager = new TilesManager(this, mHUDConnectivityManager);
+        connectivityManager = (HUDConnectivityManager) HUDOS.getHUDService(HUDOS.HUD_CONNECTIVITY_SERVICE);
+        locationService = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        headingManager = (HUDHeadingManager) HUDOS.getHUDService(HUDOS.HUD_HEADING_SERVICE);
+        tileManager = new TilesManager(this, connectivityManager);
 
         System.load("/system/lib/libreconinstruments_jni.so");
     }
@@ -62,16 +44,16 @@ public class MapActivity extends Activity implements HeadLocationListener, ITile
     protected void onResume() {
         super.onResume();
 
-        mLocationService.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, tileManager);
-        mHUDHeadingManager.register(this);
+        locationService.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, tileManager);
+        headingManager.register(this);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
 
-        mLocationService.removeUpdates(tileManager);
-        mHUDHeadingManager.unregister(this);
+        locationService.removeUpdates(tileManager);
+        headingManager.unregister(this);
     }
 
     float currentYaw = 0;
@@ -89,11 +71,11 @@ public class MapActivity extends Activity implements HeadLocationListener, ITile
         int mid = 256 * 3 / 2;
         matrix.postRotate(currentYaw, mid, mid);
 
-        int dx = -(3 * 256 - mImage.getWidth()) / 2;
-        int dy = -(3 * 256 - mImage.getHeight()) / 2;
+        int dx = -(3 * 256 - imageView.getWidth()) / 2;
+        int dy = -(3 * 256 - imageView.getHeight()) / 2;
         matrix.postTranslate(dx, dy);
 
-        mImage.setImageMatrix(matrix);
+        imageView.setImageMatrix(matrix);
     }
 
     @Override
@@ -107,6 +89,6 @@ public class MapActivity extends Activity implements HeadLocationListener, ITile
             canvas.drawBitmap(bitmaps[i], x * 256, y * 256, null);
         }
 
-        mImage.setImageBitmap(combo);
+        imageView.setImageBitmap(combo);
     }
 }
